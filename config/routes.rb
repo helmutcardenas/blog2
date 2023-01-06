cea08121e8b7243e6e3c5a2e07d0424bd0b92d95
helmut cardenas <cardenas.hcconstrucciones@gmail.com>
2023-01-06 20:03:32 -0300

    creacion de controladores y vistas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/work'
+  get 'pages/service'
+  get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'pages/home'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
