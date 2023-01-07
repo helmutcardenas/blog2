b8cd719ede67ee2ffe9b4f086aa86cf866a34ef0
helmut cardenas <cardenas.hcconstrucciones@gmail.com>
2023-01-06 21:50:45 -0300

    creacion de turbo devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'pages/service'
   get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'pages/home' #vista raiz
+  get 'pages/home'            #vista raiz
   # Defines the root path route ("/")
   root "pages#home"
 end
